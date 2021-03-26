f2ae32ef19387e30247967a2f573bc5caec32e3b
Jared Stoffan <jstoffan@box.com>
2021-03-26 16:41:32 -0700

    feat(preview): Update file loading icons for preview

--- src/components/preview/previewIcons.ts
+++ src/components/preview/previewIcons.ts
@@ -1,19 +1,25 @@
+/**
+ * Note: This map is intentionally kept separate from other icon maps, such as FileIcon. Preview only shows
+ * file-specific loading icons for file types that can be previewed. Unsupported files show a default
+ * icon, even if they have a relevant thumbnail/icon that is displayed within the item list, for example.
+ */
 import * as vars from '../../styles/variables';
 import IconFileAudio from '../../icon/content/FileAudio32';
 import IconFileBoxNote from '../../icon/content/FileBoxNote32';
 import IconFileCode from '../../icon/content/FileCode32';
 import IconFileDefault from '../../icon/content/FileDefault32';
-import IconFileDocument from '../../icon/content/FileWord32';
 import IconFileDwg from '../../icon/content/FileDwg32';
 import IconFileExcelSpreadsheet from '../../icon/content/FileExcel32';
 import IconFileGoogleDocs from '../../icon/content/FileDocs32';
 import IconFileGoogleSheets from '../../icon/content/FileSheets32';
 import IconFileGoogleSlides from '../../icon/content/FileSlides32';
+import IconFileIllustrator from '../../icon/content/FileIllustrator32';
 import IconFileImage from '../../icon/content/FileImage32';
 import IconFileKeynote from '../../icon/content/FileKeynote32';
 import IconFileNumbers from '../../icon/content/FileNumbers32';
 import IconFilePDF from '../../icon/content/FilePdf32';
 import IconFilePages from '../../icon/content/FilePages32';
+import IconFilePhotoshop from '../../icon/content/FilePhotoshop32';
 import IconFilePowerpoint from '../../icon/content/FilePowerpoint32';
 import IconFilePresentation from '../../icon/content/FilePresentation32';
 import IconFileSpreadsheet from '../../icon/content/FileSpreadsheet32';
@@ -21,6 +27,7 @@ import IconFileText from '../../icon/content/FileText32';
 import IconFileThreeD from '../../icon/content/FileThreeD32';
 import IconFileVector from '../../icon/content/FileVector32';
 import IconFileVideo from '../../icon/content/FileVideo32';
+import IconFileWord from '../../icon/content/FileWord32';
 import IconFileZip from '../../icon/content/FileZip32';
 import { SVGProps } from '../accessible-svg/AccessibleSVG';
 
@@ -95,20 +102,19 @@ register(IconFileCode, '#E33D55', [
     'sh',
     'vi',
     'vim',
-    'webdoc',
+    'xhtml',
     'xml',
     'xsd',
     'xsl',
     'yaml',
 ]);
 register(IconFileDwg, '#009AED', ['dwg']);
-register(IconFileDocument, '#185ABD', ['doc', 'docx']);
+register(IconFileIllustrator, '#FF9400', ['ai']);
 register(IconFileExcelSpreadsheet, '#107C41', ['xls', 'xlsm', 'xlsx', 'xlsb']);
 register(IconFileGoogleDocs, '#4083F7', ['gdoc']);
 register(IconFileGoogleSheets, '#21A464', ['gsheet']);
-register(IconFileGoogleSlides, '#F7BA00', ['gslide']);
+register(IconFileGoogleSlides, '#F7BA00', ['gslide', 'gslides']);
 register(IconFileImage, '#3FB87F', [
-    'ai',
     'bmp',
     'cr2',
     'crw',
@@ -121,7 +127,6 @@ register(IconFileImage, '#3FB87F', [
     'nef',
     'png',
     'ps',
-    'psd',
     'raf',
     'raw',
     'svg',
@@ -134,10 +139,11 @@ register(IconFileKeynote, '#007AFF', ['key']);
 register(IconFileNumbers, '#00A650', ['numbers']);
 register(IconFilePDF, '#D0021B', ['pdf']);
 register(IconFilePages, '#FF9500', ['pages']);
+register(IconFilePhotoshop, '#26C9FF', ['psd']);
 register(IconFilePowerpoint, '#C43E1C', ['ppt', 'pptx']);
 register(IconFilePresentation, '#F7931D', ['odp']);
 register(IconFileSpreadsheet, '#3FB87F', ['csv', 'tsv']);
-register(IconFileText, vars.bdlBoxBlue, ['log', 'msg', 'ods', 'rtf', 'txt', 'wpd']);
+register(IconFileText, vars.bdlBoxBlue, ['log', 'msg', 'ods', 'odt', 'rtf', 'txt', 'webdoc', 'wpd']);
 register(IconFileThreeD, '#F7931D', ['3ds', 'box3d', 'dae', 'fbx', 'obj', 'ply', 'stl']);
 register(IconFileVector, '#F7931D', ['eps']);
 register(IconFileVideo, '#009AED', [
@@ -159,6 +165,7 @@ register(IconFileVideo, '#009AED', [
     'ts',
     'wmv',
 ]);
+register(IconFileWord, '#185ABD', ['doc', 'docx']);
 register(IconFileZip, vars.bdlGray20, ['tgz', 'zip']);
 
 export const getColor = (extension?: string): Config['color'] => {
